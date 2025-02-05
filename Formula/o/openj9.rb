9d5737d1d711ae2f51b1ea4ee9238a7d4e22a5c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-05 08:13:06 +0000

    openj9 0.49.0

--- Formula/o/openj9.rb
+++ Formula/o/openj9.rb
@@ -2,8 +2,8 @@ class Openj9 < Formula
   desc "High performance, scalable, Java virtual machine"
   homepage "https://www.eclipse.org/openj9/"
   url "https://github.com/eclipse-openj9/openj9.git",
-      tag:      "openj9-0.48.0",
-      revision: "1d5831436ec378c7dd9f57415bec39d3f5817d57"
+      tag:      "openj9-0.49.0",
+      revision: "3c3d179854a524d7f95225999169ee09fda46033"
   license any_of: [
     "EPL-2.0",
     "Apache-2.0",
@@ -83,8 +83,8 @@ class Openj9 < Formula
 
   resource "omr" do
     url "https://github.com/eclipse-openj9/openj9-omr.git",
-        tag:      "openj9-0.48.0",
-        revision: "d10a4d553a3cfbf35db0bcde9ebccb24cdf1189f"
+        tag:      "openj9-0.49.0",
+        revision: "e49875871c2862e0d132e3695d55273bfbac08b6"
   end
 
   resource "openj9-openjdk-jdk" do
